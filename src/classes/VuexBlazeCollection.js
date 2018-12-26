13ec5db1b4298541406bc63b8288aa364f77e8a9
Yoshinori Ishii <yoshinori.ishii@gmail.com>
2018-12-26 19:55:44 +0900

    Separate CRUD functions from collection actions

--- src/classes/VuexBlazeCollection.js
+++ src/classes/VuexBlazeCollection.js
@@ -1,10 +1,13 @@
 import { actionNamer } from '../utils'
-import { isString } from 'lodash'
+import { isString, set } from 'lodash'
 import { VUEXBLAZE_IGNORE_ON_UNCONTROLLABLE_CHANGE } from '../options'
 import VuexBlazeDoc from './VuexBlazeDoc'
 import VuexBlazeCollectionBinder from './VuexBlazeCollectionBinder'
 import VuexBlazeConfig from './VuexBlazeConfig'
 import VuexBlazeQueryBuilder from './VuexBlazeQueryBuilder'
+import VuexBlazeDocSnapshotProcessor from './VuexBlazeDocSnapshotProcessor'
+import VuexBlazePath from './VuexBlazePath'
+import { singular } from 'pluralize'
 
 export default class VuexBlazeCollection {
   constructor(collectionName) {
@@ -18,6 +21,74 @@ export default class VuexBlazeCollection {
 
   bind(userOptions) {
     return this.bindTo(this.collectionName, userOptions)
+  }
+
+  crud(userBaseName, userOptions) {
+    const options = { ...this.DEFAULT_OPTIONS, ...userOptions }
+    const baseName = userBaseName || singular(this.collectionName)
+    const actionName = actionNamer(VuexBlazeConfig.get('actionNameCase'))
+    const getRefFromContext = context => {
+      const $firestore = context.rootGetters['vuexblaze/$firestore']
+      return $firestore.collection(this.collectionName)
+    }
+
+    return {
+      [actionName('create', baseName)]: async (context, payload) => {
+        const collectionRef = getRefFromContext(context)
+        if (Array.isArray(payload)) {
+          const [id, data] = payload
+          await collectionRef.doc(id).set(data)
+        } else if (payload.id) {
+          const id = payload.id
+          const data = { ...payload }
+          delete data.id
+          await collectionRef.doc(id).set(data)
+        } else {
+          await collectionRef.add(payload)
+        }
+      },
+      [actionName('retrieve', baseName)]: async (context, id) => {
+        const collectionRef = getRefFromContext(context)
+        const snapshot = await collectionRef.doc(id).get()
+
+        const processor = new VuexBlazeDocSnapshotProcessor(
+          null,
+          snapshot,
+          VuexBlazePath.createRoot(options),
+          options
+        )
+        processor.process()
+        await Promise.all(
+          Object.values(processor.refObservers).map(observer => {
+            observer.onChange(change => {
+              set(
+                processor.doc,
+                change.observer.path.get(),
+                change.observer.currentDoc
+              )
+              observer.stop()
+            })
+            return observer.observe()
+          })
+        )
+        return processor.doc
+      },
+      [actionName('update', baseName)]: async (context, payload) => {
+        const collectionRef = getRefFromContext(context)
+        const [id, data] = Array.isArray(payload)
+          ? payload
+          : [payload.id, { ...payload }]
+        delete data.id
+        if (!id) throw new Error('"id" is required to update doc')
+        await collectionRef.doc(id).set(data, { merge: true })
+      },
+      [actionName('delete', baseName)]: async (context, payload) => {
+        const collectionRef = getRefFromContext(context)
+        const id = isString(payload) ? payload : payload.id
+        if (!id) throw new Error('"id" is required to update doc')
+        await collectionRef.doc(id).delete()
+      }
+    }
   }
 
   bindTo(stateName, userOptions) {
@@ -51,33 +122,6 @@ export default class VuexBlazeCollection {
       [actionName('reload', stateName)]: async () => {
         if (!binder) throw new Error('Not binded')
         await binder.reload()
-      },
-      [actionName('updateDocIn', stateName)]: async (context, payload) => {
-        if (!binder) throw new Error('Not binded')
-        const [id, data] = Array.isArray(payload)
-          ? payload
-          : [payload.id, { ...payload }]
-        delete data.id
-        if (!id) throw new Error('"id" is required to update doc')
-        await binder.collectionRef.doc(id).set(data, { merge: true })
-      },
-      [actionName('addDocTo', stateName)]: async (context, payload) => {
-        if (Array.isArray(payload)) {
-          const [id, data] = payload
-          await binder.collectionRef.doc(id).set(data)
-        } else if (payload.id) {
-          const id = payload.id
-          const data = { ...payload }
-          delete data.id
-          await binder.collectionRef.doc(id).set(data)
-        } else {
-          await binder.collectionRef.add(payload)
-        }
-      },
-      [actionName('deleteDocFrom', stateName)]: async (context, payload) => {
-        const id = isString(payload) ? payload : payload.id
-        if (!id) throw new Error('"id" is required to update doc')
-        await binder.collectionRef.doc(id).delete()
       }
     }
   }
